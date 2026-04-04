db93bf749b854d1307443d697a5bd4663f1ba03b
Stefan Andersson <stefan.andersson@sveki.se>
2026-04-04 14:46:12 +0200

    Add variables

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL =
-  `${window.location.protocol}//${window.location.hostname}:8080`;
+const isProd = import.meta.env.PROD;
+
+export const API_BASE_URL = isProd
+  ? import.meta.env.VITE_API_BASE_URL
+  : `${window.location.protocol}//${window.location.hostname}:8080`;
